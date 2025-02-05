b5848fd979b828eca08f4156fe514adab2cd96e1
20481A5450 <shaikzohaibgec@gmail.com>
2025-02-05 17:41:37 +0530

    update: modify Next.js configuration for static export and adjust package.json scripts

--- next.config.js
+++ next.config.js
@@ -4,15 +4,15 @@ const nextConfig = {
   experimental: {
     runtime: 'edge',
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      };
-    }
-    return config;
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     config.experiments = {
+  //       ...config.experiments,
+  //       topLevelAwait: true,
+  //     };
+  //   }
+  //   return config;
+  // },
   output: "export", // Ensures static export
   basePath: "/Portfolio-main", // Must match your GitHub repository name
   assetPrefix: "/Portfolio-main", // Should match the basePath
