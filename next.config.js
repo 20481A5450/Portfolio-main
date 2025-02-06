f82623a7406950af789c38177d815b21c598fd4a
20481A5450 <shaikzohaibgec@gmail.com>
2025-02-06 10:19:17 +0530

    update: rename Next.js workflow file and configure output and base path in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,  
+  output: "export",
+  basePath: "/Portfolio-main", 
   // Add Cloudflare Workers configuration
   experimental: {
     // runtime: 'edge',
