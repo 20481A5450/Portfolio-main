98183f5ea9d9615ee01d57019bc7478a4828631f
20481A5450 <shaikzohaibgec@gmail.com>
2025-02-06 11:32:15 +0530

    update: enhance GitHub Actions workflow for Next.js deployment and configure asset paths for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.NODE_ENV === 'production';
+const repoName = 'Portfolio-main'; // Replace with your GitHub repo name
+
+module.exports = {
+  output: 'export',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  basePath: isGithubPages ? `/${repoName}` : '',
+};
+
 const nextConfig = {
-  output: "export", // Enables static export
-  images: {
-    unoptimized: true, // Required since image optimization requires a server
+  reactStrictMode: true,
+  // Add Cloudflare Workers configuration
+  // experimental: {
+  //   runtime: 'edge',
+  // },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+    }
+    return config
   },
 };
 
